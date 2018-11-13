84a3db15999678c6f62783449c4eb2e1b869ee11
Hector <hector.hc2014@gmail.com>
2018-11-13 23:27:04 +0000

    Java ya se abre por consola y con parámetros

--- Java/Misioneros_Canibales/src/misioneros_canibales/Misioneros_Canibales.java
+++ Java/Misioneros_Canibales/src/misioneros_canibales/Misioneros_Canibales.java
@@ -5,6 +5,10 @@
  */
 package misioneros_canibales;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 
 
 /**
@@ -17,19 +21,25 @@ public class Misioneros_Canibales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        List <String> argumentos= new LinkedList();
+        
+        for(int i =0; i< args.length;i++){
+            argumentos.add(args[i]);
+            System.out.println(args[i]);
+        }
+        boolean flag=false;
+        if(argumentos.contains("-t")){
+            argumentos.remove("-t");
+            flag=true;
+        }
         StockProcessor prueba= new StockProcessor();
-        prueba.misioneros_CanibalesT("C:\\Users\\Héctor\\Desktop\\CaM-PR3\\Java\\Misioneros_Canibales\\fichero_básico.txt");
-        //prueba.misioneros_Canibales("fichero10_1.txt");
-        //prueba.misioneros_Canibales("fichero20_2.txt");
-        //prueba.misioneros_CanibalesT("fichero50_5.txt");
-        //prueba.misioneros_CanibalesT("fichero100_10.txt");
-        //prueba.misioneros_CanibalesT("fichero200_20.txt");
-        //prueba.misioneros_CanibalesT("fichero500_50.txt");
-        //prueba.misioneros_CanibalesT("fichero1000_100.txt");
-        
-        //prueba.misioneros_CanibalesT("fichero_básico.txt");
-        //prueba.misioneros_CanibalesT("fichero_avanzado.txt");
-        
+        Iterator <String> iterador= argumentos.iterator();
+        String aux="";
+        while(iterador.hasNext()){
+            aux= iterador.next();
+            prueba.misioneros_Canibales(aux, flag);
+        }
+       
         
     }
     
--- Java/Misioneros_Canibales/src/misioneros_canibales/StockProcessor.java
+++ Java/Misioneros_Canibales/src/misioneros_canibales/StockProcessor.java
@@ -39,74 +39,8 @@ public class StockProcessor {
         return result;
     }
     
-    public void misioneros_Canibales(String fichero){
-        List<String> a = FileTextReader.read(fichero);
-        Iterator b= a.iterator();
-        int iteraciones=0;
-        List <Long> tiempos= new LinkedList();
-        while(b.hasNext()){
-            System.out.println("############################==" + 
-                    iteraciones + "==#############################");
-            System.out.println("Iteración: "+ iteraciones);
-            iteraciones++;
-            String aux= (String) b.next();
-            List<String> orillaA= FileTextReader.toInt(aux);
-            List<String> orillaB= new LinkedList();
-            
-            if (numero_canibales(orillaA) > numero_misioneros(orillaA)){
-                System.out.println("El número de canibales no puede superar al de misioneros");
-                continue;
-            }
-
-            if (orillaA.size()%3!=0){
-                System.out.println("NO ES MULTIPLO DE 3");
-                continue;
-            }
-            System.out.println("El número de misoneros: " + numero_misioneros(orillaA));
-            System.out.println("El número de canibles: " + numero_canibales(orillaA));
-            Iterator p= orillaA.iterator();
-            
-            while(p.hasNext()){
-                System.out.println("ORILLA_A -->"+orillaA.size());
-                System.out.println("ORILLA_B -->"+orillaB.size());
-                
-                IteradorCombinacion it = new IteradorCombinacion(orillaA, 3);
-                Iterator s = it.iterator();
-                while (s.hasNext()) {
-                    List t= (List) s.next();
-                    List<String> auxA= clone(orillaA);
-                    List<String> auxB= clone(orillaB);
-                    Iterator ti= t.iterator();
-                    while(ti.hasNext()){
-                        String auxTi= (String) ti.next();
-                        auxA.remove(auxTi);
-                        auxB.add(auxTi);
-                    }
-                    if ((numero_misioneros(t)>numero_canibales(t) || numero_misioneros(t)==0)
-                            && numero_misioneros(auxA)!=1
-                            && (numero_canibales(auxA)<= numero_misioneros(auxA) || numero_misioneros(auxA)==0)
-                            && (numero_canibales(auxB) <= numero_misioneros(auxB))){
-                        System.out.println("Combinacion ganadoras:"+ t);
-                        Iterator ti2= t.iterator();
-                        while(ti2.hasNext()){
-                            String auxTi2=  (String) ti2.next();
-                            orillaA.remove(auxTi2);
-                            orillaB.add(auxTi2);
-                        }
-                        break;
-                        
-                    }
-                
-                }
-                
-            }
-            System.out.println("Resultado de la orilla A: "+ orillaA);
-            System.out.println("Resultado de la orilla B: "+ orillaB);
-        }
-       
-        
-    }
-        public void misioneros_CanibalesT(String fichero){
+    
+        public void misioneros_Canibales(String fichero, boolean timeFlag){
         List<String> a = FileTextReader.read(fichero);
         Iterator b= a.iterator();
         int iteraciones=0;
@@ -123,8 +57,8 @@ public class StockProcessor {
             
             if (numero_canibales(orillaA) > numero_misioneros(orillaA)){
                 System.out.println("El número de canibales no puede superar al de misioneros");
-                System.out.println(numero_misioneros(orillaA));
-                System.out.println(numero_canibales(orillaA));
+                System.out.println("El número de misoneros: " + numero_misioneros(orillaA));
+                System.out.println("El número de misoneros: " + numero_canibales(orillaA));
                 continue;
             }
             if (orillaA.size()%3!=0){
@@ -133,14 +67,20 @@ public class StockProcessor {
                 System.out.println(numero_canibales(orillaB));
                 continue;
             }
+            
             System.out.println("El número de misoneros: " + numero_misioneros(orillaA));
             System.out.println("El número de canibles: " + numero_canibales(orillaA));
+            
+
             Iterator p= orillaA.iterator();
             boolean flag= true;
             while(p.hasNext()){
                 int sizeFlag=orillaA.size();
-                System.out.println("ORILLA_A -->"+orillaA.size());
-                System.out.println("ORILLA_B -->"+orillaB.size());
+                if(!timeFlag){
+                    System.out.println("ORILLA_A -->"+orillaA.size());
+                    System.out.println("ORILLA_B -->"+orillaB.size());
+                }
+                
                 
                 IteradorCombinacion it = new IteradorCombinacion(orillaA, 3);
                 Iterator s = it.iterator();
@@ -158,7 +98,10 @@ public class StockProcessor {
                             && numero_misioneros(auxA)!=1
                             && (numero_canibales(auxA)<= numero_misioneros(auxA) || numero_misioneros(auxA)==0)
                             && (numero_canibales(auxB) <= numero_misioneros(auxB))){
-                        System.out.println("Combinacion ganadoras:"+ t);
+                        if (!timeFlag){
+                            System.out.println("Combinacion ganadoras:"+ t);
+                        }
+                        
                         Iterator ti2= t.iterator();
                         while(ti2.hasNext()){
                             String auxTi2=  (String) ti2.next();
@@ -180,9 +123,14 @@ public class StockProcessor {
             }
             if (flag){
                 long tiempoIter= System.nanoTime()-tiempoInicio;
-                System.out.println("Resultado de la orilla A: "+ orillaA);
-                System.out.println("Resultado de la orilla B: "+ orillaB);
-                System.out.println("Tiempo en milisegundos: "+tiempoIter/1e6);
+                if (!timeFlag){
+                    System.out.println("Resultado de la orilla A: "+ orillaA);
+                    System.out.println("Resultado de la orilla B: "+ orillaB);
+                }else{
+                    System.out.println("Tiempo en milisegundos: "+tiempoIter/1e6);
+                }
+                
+                
                 tiempos.add(tiempoIter);
             }
             
@@ -192,7 +140,10 @@ public class StockProcessor {
         while(tiempoIt.hasNext()){
             tiempoFinal+=(long)tiempoIt.next();
         }
-        System.out.println("Tiempo final en segundos:" + tiempoFinal/1e9);
+        if(timeFlag){
+            System.out.println("Tiempo final en segundos:" + tiempoFinal/1e9);
+        }
+        
         
     }
 }
